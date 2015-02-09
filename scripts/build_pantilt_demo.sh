d422455d4ec5596e2d2a44d6bc2566d37273b3d2
John Leimon <jleimon@gmail.com>
2015-02-08 23:04:25 -0600

    Fix typo in script.

--- scripts/build_pantilt_demo.sh
+++ scripts/build_pantilt_demo.sh
@@ -5,6 +5,6 @@ TARGET_BUILD_FOLDER=../build
 mkdir $TARGET_BUILD_FOLDER
 mkdir $TARGET_BUILD_FOLDER/pantilt
 
-cd $TARGET_BUILD_FOLDER/pan_tilt_demo
-cmake ../../src/host/pan_tilt_demo
+cd $TARGET_BUILD_FOLDER/pantilt
+cmake ../../src/host/pantilt
 make
