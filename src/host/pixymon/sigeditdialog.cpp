1e9904df7b8c518ace6523cddd05c00f8368c292
Jesse Crossen <jesse.crossen@gmail.com>
2016-01-16 23:28:49 -0500

    Correct colorspace transform and show signatures.

--- src/host/pixymon/sigeditdialog.cpp
+++ src/host/pixymon/sigeditdialog.cpp
@@ -8,6 +8,27 @@
 #include <QDebug>
 #include "renderer.h"
 #include "calc.h"
+#include "string.h"
+
+// COLORSPACE UTILS ***********************************************************
+
+#define UV_SCALE_MAX ((0xFF << CL_LUT_ENTRY_SCALE) / 0xFF)
+
+inline void rgb2yuv(uint32_t rgb, int32_t *y, int32_t *u, int32_t *v) {
+    int r = (rgb >> 16) & 0xFF;
+    int g = (rgb >> 8) & 0xFF;
+    int b = rgb & 0xFF;
+    *y = r + g + b;
+    if (*y == 0) {
+        *u = *v = 0;
+    }
+    else {
+        *u = ((r - g) << CL_LUT_ENTRY_SCALE) / *y;
+        *v = ((b - g) << CL_LUT_ENTRY_SCALE) / *y;
+    }
+}
+
+// DIALOG *********************************************************************
 
 SigEditDialog::SigEditDialog(QWidget *parent, Interpreter *interpreter) :
     QDialog(parent),
@@ -35,30 +56,30 @@ void SigEditDialog::handleImage(QImage image, uchar renderFlags)
     m_framecount++;
     if (m_framecount == 1) {
         // set up the map image
-        QImage map(512, 512, QImage::Format_ARGB32);
+        int size = 512;
+        QImage map(size, size, QImage::Format_ARGB32);
+        map.fill(QColor(0, 0, 0, 0)); // transparency
         QPainter painter;
         painter.begin(&map);
         painter.setPen(Qt::NoPen);
         QRect spot;
-        // fill the map with transparency
-        map.fill(QColor(0, 0, 0, 0));
         // transfer the camera image onto the colorspace map
-        int x, y, r, g, b;
-        int32_t u, v;
+        int px, py;
+        int32_t y, u, v;
         uint *p;
-        for (y = 0; y < image.height(); y++) {
-            p = (uint *)image.scanLine(y - 1);
-            for (x = 0; x < image.width(); x++) {
-                // get color components
-                r = (*p >> 16) & 0xFF;
-                g = (*p >> 8) & 0xFF;
-                b = *p & 0xFF;
-                // convert into (u, v) space
-                u = (r - g) + 0xFF;
-                v = (b - g) + 0xFF;
+        for (py = 0; py < image.height(); py++) {
+            p = (uint *)image.scanLine(py - 1);
+            for (px = 0; px < image.width(); px++) {
+                // project into the colorspace
+                rgb2yuv(*p, &y, &u, &v);
+                // scale onto the image
+                u = ((u + UV_SCALE_MAX) * size) / (UV_SCALE_MAX * 2);
+                v = ((v + UV_SCALE_MAX) * size) / (UV_SCALE_MAX * 2);
                 // draw a spot on the map
-                spot = QRect(u - 2, v - 2, 4, 4);
-                painter.fillRect(spot, *p);
+                if (y > SigModule::m_yMin) {
+                    spot = QRect(u - 2, v - 2, 4, 4);
+                    painter.fillRect(spot, *p);
+                }
                 p++;
             }
         }
@@ -75,20 +96,132 @@ void SigEditDialog::handleImage(QImage image, uchar renderFlags)
 
 void SigEditDialog::paintEvent(QPaintEvent *event)
 {
-    int i;
-    QPainter p(this);
+    int i, j, y, u, v;
+    QPainter painter(this);
     QPixmap pixmap;
-    QRect r = this->ui->imageLabel->geometry();
-    // fill the background with middle gray so the colors stand out
-    p.fillRect(r, QColor(0x80, 0x80, 0x80));
+    QRect xybounds = this->ui->imageLabel->geometry();
+    // map coordinates into (u, v) space
+    painter.translate(xybounds.center().x(), xybounds.center().y());
+    qreal xscale = (qreal)xybounds.width() / (qreal)(2 * UV_SCALE_MAX);
+    qreal yscale = (qreal)xybounds.height() / (qreal)(2 * UV_SCALE_MAX);
+    painter.scale(xscale, yscale);
+    QRect uvbounds = QRect(QPoint(- UV_SCALE_MAX, - UV_SCALE_MAX),
+                     QPoint(UV_SCALE_MAX, UV_SCALE_MAX));
+    // draw a background showing the extents of the uv prjection
+    //  with reference colors at the edges
+    uint32_t refcolors[6] = { 0xFF0000, 0xFFFF00, 0x00FF00,
+                              0x00FFFF, 0x0000FF, 0xFF00FF };
+    QPoint corners[6];
+    painter.setPen(Qt::NoPen);
+    for (i = 0; i < 6; i++) {
+        rgb2yuv(refcolors[i], &y, &u, &v);
+        corners[i] = QPoint(u, v);
+    }
+    // draw the background
+    painter.setBrush(QColor(0x80, 0x80, 0x80));
+    painter.drawPolygon(corners, 6);
+    // draw the edge
+    painter.setBrush(Qt::NoBrush);
+    QPen edgePen;
+    j = 5;
+    for (i = 0; i < 6; i++) {
+        QLinearGradient grad = QLinearGradient(QPointF(corners[i]), QPointF(corners[j]));
+        grad.setColorAt(0.0, refcolors[i]);
+        grad.setColorAt(1.0, refcolors[j]);
+        edgePen = QPen(QBrush(grad), 3);
+        edgePen.setCosmetic(true);
+        edgePen.setCapStyle(Qt::RoundCap);
+        painter.setPen(edgePen);
+        painter.drawLine(corners[i], corners[j]);
+        j = i;
+    }
     // draw the stack of colorspace map frames
     for (i = 0; i < m_images.count(); i++) {
         pixmap.convertFromImage(m_images[i]);
-        p.drawPixmap(r, pixmap);
+        painter.drawPixmap(uvbounds, pixmap);
+    }
+    // draw signature boxes
+    ColorSignature *sig;
+    QRect main;
+    QRect scaled;
+    float range;
+    QPen sigPen = QPen(QColor("white"), 2);
+    sigPen.setCosmetic(true);
+    QBrush sigBrush = QBrush(QColor(255, 255, 255, 64));
+    for (i = 0; i < CL_NUM_SIGNATURES; i++) {
+        sig = &(SigModule::m_signatures[i]);
+        main = QRect(QPoint(sig->m_uMin, sig->m_vMin),
+                     QPoint(sig->m_uMax, sig->m_vMax));
+        range = SigModule::m_ranges[i];
+        scaled.setLeft(sig->m_uMean + (sig->m_uMin - sig->m_uMean) * range);
+        scaled.setRight(sig->m_uMean + (sig->m_uMax - sig->m_uMean) * range);
+        scaled.setTop(sig->m_vMean + (sig->m_vMin - sig->m_vMean) * range);
+        scaled.setBottom(sig->m_vMean + (sig->m_vMax - sig->m_vMean) * range);
+        // draw the area after applying the scale factor
+        painter.setBrush(sigBrush);
+        painter.setPen(Qt::NoPen);
+        painter.drawRect(scaled);
+        // draw the core area of the signature
+        painter.setBrush(Qt::NoBrush);
+        painter.setPen(sigPen);
+        painter.drawRect(main);
     }
 }
 
 SigEditDialog::~SigEditDialog()
 {
     delete ui;
+}
+
+// MONMODULE IMPLEMENTATION ***************************************************
+
+MON_MODULE(SigModule)
+
+ColorSignature SigModule::m_signatures[CL_NUM_SIGNATURES];
+float SigModule::m_ranges[CL_NUM_SIGNATURES];
+int32_t SigModule::m_yMin;
+
+SigModule::SigModule(Interpreter *interpreter) : MonModule(interpreter)
+{
+    // clear signatures and ranges
+    memset(m_signatures, 0, sizeof(m_signatures));
+    memset(m_ranges, 0, sizeof(m_ranges));
+    m_yMin = 0;
+}
+
+SigModule::~SigModule() { }
+
+bool SigModule::render(uint32_t fourcc, const void *args[])
+{
+    return(false);
+}
+
+void SigModule::paramChange()
+{
+    int i;
+    QString id;
+    QVariant val;
+    QByteArray ba;
+    uint32_t sigLen;
+    uint8_t *sigData;
+    // get all existing signatures and ranges
+    for (i = 0; i < CL_NUM_SIGNATURES; i++) {
+        id.sprintf("signature%d", i + 1);
+        val = pixyParameter(id);
+        ba = val.toByteArray();
+        Chirp::deserialize((uint8_t *)ba.data(), ba.size(), &sigLen, &sigData, END);
+        if (sigLen == sizeof(ColorSignature)) {
+            memcpy(&(m_signatures[i]), sigData, sizeof(ColorSignature));
+        }
+        id.sprintf("Signature %d range", i + 1);
+        val = pixyParameter(id);
+        if (val.canConvert(QVariant::Double)) {
+            m_ranges[i] = val.toFloat();
+        }
+    }
+    // get the minimum brightness
+    val = pixyParameter("Min brightness");
+    if (val.canConvert(QVariant::Double)) {
+        m_yMin = val.toFloat() * (3 * 0xFF);
+    }
 }
--- src/host/pixymon/sigeditdialog.h
+++ src/host/pixymon/sigeditdialog.h
@@ -4,12 +4,30 @@
 #include <QDialog>
 #include <QPixmap>
 #include "monmodule.h"
+#include "colorlut.h"
 
 class Interpreter;
 
 namespace Ui {
-class SigEditDialog;
+    class SigEditDialog;
+    class SigModule;
 }
+
+class SigModule : public MonModule
+{
+public:
+    SigModule(Interpreter *interpreter);
+    ~SigModule();
+    virtual bool render(uint32_t fourcc, const void *args[]);
+    virtual void paramChange();
+
+    // current signatures
+    static ColorSignature m_signatures[CL_NUM_SIGNATURES];
+    // current signature range multipliers
+    static float m_ranges[CL_NUM_SIGNATURES];
+    // minimum brightness
+    static int32_t m_yMin;
+};
 
 class SigEditDialog : public QDialog
 {
