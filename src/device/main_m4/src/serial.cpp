8e359961d4b89a519220b14a944914ca241109fa
Andrewiski <adevries@digitalexample.com>
2017-12-21 10:57:11 -0500

    Added FTC I2C as PixyMon Option

--- src/device/main_m4/src/serial.cpp
+++ src/device/main_m4/src/serial.cpp
@@ -175,6 +175,183 @@ uint16_t lego_getData(uint8_t *buf, uint32_t buflen)
 #endif
 		return 6;
 	}
+
+
+else{
+#if 0
+		static uint8_t c = 0;
+
+		buf[0] = c++;
+#else
+		//printf("%x\n", c);
+
+		if (c==0x42) // this works in port view mode on the ev3's LCD
+		{
+			BlobA *max;
+			max = g_blobs->getMaxBlob();
+			if (max==0 || max==(BlobA *)-1)
+				buf[0] = 0;
+			else
+			{
+				width = max->m_right - max->m_left;
+				temp = ((max->m_left + width/2)*819)>>10;
+				buf[0] = temp;
+			}
+		}
+		else
+			buf[0] = 1;	 // need to return nonzero value for other inquiries or LEGO brick will think we're an analog sensor
+
+#endif
+		return 1;
+	}
+}
+
+uint16_t ftc_getData(uint8_t *buf, uint32_t buflen)
+{
+	uint8_t c;
+	uint16_t d;
+	uint16_t numBlobs;
+	BlobA* ftcBlobs;
+	uint32_t temp, width, height;
+	Iserial *serial = ser_getSerial();
+
+	if (serial->receive(&c, 1)==0)
+		return 0;
+
+#if 1
+	if (c==0x00)
+	{
+		//printf("0\n");
+		char *str = "V0.1";
+		strcpy((char *)buf, str);
+		return 5;
+		//return strlen((char *)str);
+	}
+	if (c==0x08)
+	{
+		//printf("8\n");
+		char *str = "Pixy";
+		strcpy((char *)buf, str);
+		return 5;
+		//return strlen((char *)str);
+	}
+	else if (c==0x10)
+	{
+		//printf("10\n");
+		char *str = "Pixy";
+		strcpy((char *)buf, str);
+		return 5;
+		//return strlen((char *)str);
+	}
+	else
+#endif
+	if (c==0x50)
+	{
+		BlobB *max;
+#if 0
+		buf[0] = 1;
+		buf[1] = 2;
+		buf[2] = 3;
+		buf[3] = 4;
+		buf[4] = 5;
+		buf[5] = 6;
+		buf[6] = 7;
+#else
+		max = (BlobB *)g_blobs->getMaxBlob();
+		if (max==0)
+			memset(buf, 0, 7);
+		else if (max==(BlobB *)-1)
+			memset(buf, -1, 7);
+		else
+		{
+			width = max->m_right - max->m_left;
+			height = max->m_bottom - max->m_top;
+			*(uint16_t *)buf = max->m_model; // signature
+			temp = ((max->m_left + width/2)*819)>>10;
+			buf[2] = temp; // x
+			buf[3] = max->m_top + height/2; // y
+			temp = (width*819)>>10;
+			buf[4] = temp; // width
+			buf[5] = height; // height
+			if (max->m_model>CL_NUM_SIGNATURES)
+			{
+				temp = ((int32_t)max->m_angle*91)>>7;
+				g_angle = temp;
+			}
+		}
+#endif
+		return 6;
+	}
+	else if (c==0x60)
+	{
+		buf[0] = g_angle;
+		return 1;
+	}
+	else if (c>=0x51 && c<=0x57)
+	{
+#if 0
+		buf[0] = 1;
+		buf[1] = 2;
+		buf[2] = 3;
+		buf[3] = 4;
+		buf[4] = 5;
+#else
+		BlobA *max;
+		max = g_blobs->getMaxBlob(c-0x50, &numBlobs);
+		if (max==0)
+			memset(buf, 0, 5);
+		else if (max==(BlobA *)-1)
+			memset(buf, -1, 5);
+		else
+		{
+			width = max->m_right - max->m_left;
+			height = max->m_bottom - max->m_top;
+			buf[0] = numBlobs; // number of blocks that match signature
+			temp = ((max->m_left + width/2)*819)>>10;
+			buf[1] = temp; // x
+			buf[2] = max->m_top + height/2;	// y
+			temp = (width*819)>>10;
+			buf[3] = temp; // width
+			buf[4] = height; // height
+		}
+#endif
+		return 5;
+	}
+	else if (c==0x58)
+	{
+		BlobB *max;
+		if (serial->receive((uint8_t *)&d, 2)<2) // receive cc signature to look for
+			return 0;
+#if 0
+		buf[0] = 1;
+		buf[1] = 2;
+		buf[2] = 3;
+		buf[3] = 4;
+		buf[4] = 5;
+		buf[5] = 6;
+#else
+		max = (BlobB *)g_blobs->getMaxBlob(d, &numBlobs);
+		if (max==0)
+			memset(buf, 0, 6);
+		else if (max==(BlobB *)-1)
+			memset(buf, -1, 6);
+		else
+		{
+			width = max->m_right - max->m_left;
+			height = max->m_bottom - max->m_top;
+			buf[0] = numBlobs; // number of cc blocks that match
+			temp = ((max->m_left + width/2)*819)>>10;
+			buf[1] = temp; // x
+			buf[2] = max->m_top + height/2; // y
+			temp = (width*819)>>10;
+			buf[3] = temp; // width
+			buf[4] = height; // height
+			temp = ((int32_t)max->m_angle*91)>>7;
+			buf[5] = temp; // angle
+		}
+#endif
+		return 6;
+	}
 	else if (c==0x70)  //FTC Extension return top 5 Largest Signatures 27 byte limit need 5 bytes per sig 5 * 5 = 25 + NumBlocks in view = 26
 		{
 
@@ -421,15 +598,13 @@ else{
 }
 
 
-
-
 uint32_t callback(uint8_t *data, uint32_t len)
 {
 	if (g_interface==SER_INTERFACE_LEGO){
 		return lego_getData(data, len);
 	}
 	else if (g_interface==SER_INTERFACE_FTC){
-		return lego_getData(data, len);
+		return ftc_getData(data, len);
 	}
 	else
 		return g_blobs->getBlock(data, len);
@@ -452,7 +627,7 @@ void ser_loadParams()
 {
 #ifndef LEGO
 	prm_add("Data out port", 0, 
-		"Selects the port that's used to output data (default Arduino ICSP SPI) @c Interface @s 0=Arduino_ICSP_SPI @s 1=SPI_with_SS @s 2=I2C @s 3=UART @s 4=analog/digital_x @s 5=analog/digital_y @s 6=LEGO_I2C", UINT8(0), END);
+		"Selects the port that's used to output data (default Arduino ICSP SPI) @c Interface @s 0=Arduino_ICSP_SPI @s 1=SPI_with_SS @s 2=I2C @s 3=UART @s 4=analog/digital_x @s 5=analog/digital_y @s 6=LEGO_I2C @s 7=FTC_I2C", UINT8(0), END);
 	prm_add("I2C address", PRM_FLAG_HEX_FORMAT, 
 		"@c Interface Sets the I2C address if you are using I2C data out port. (default 0x54)", UINT8(I2C_DEFAULT_SLAVE_ADDR), END);
 	prm_add("UART baudrate", 0, 
@@ -513,14 +688,14 @@ int ser_setInterface(uint8_t interface)
 
 	case SER_INTERFACE_LEGO:
 		g_serial = g_i2c0;
- 		// For now comment this out tell I get to changing pixymon to pass the FTC Parmeter below g_i2c0->setSlaveAddr(0x01);
+ 		g_i2c0->setSlaveAddr(0x01);
 		g_i2c0->setFlags(true, false);
 		break;
 	case SER_INTERFACE_FTC:
-			g_serial = g_i2c0;
-	 		//g_i2c0->setSlaveAddr(0x01);
-			g_i2c0->setFlags(true, false);
-			break;
+		g_serial = g_i2c0;
+	 	// Use the Address set via PixyMon g_i2c0->setSlaveAddr(0x01);
+		g_i2c0->setFlags(true, false);
+		break;
 
 	default:
 	case SER_INTERFACE_ARDUINO_SPI:
